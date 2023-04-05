bf17d5c301ae73a6bd28d42ddeaa20781339a7e6
debcrisjol <debcrisjol@gmail.com>
2023-04-05 16:59:41 +0100

    aborterror

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,9 @@
 import { useState,useEffect } from "react";
+import useFetch from "./useFetch";
 import BlogList from './BlogList';
 const Home = () => {
-  const [blogs,setBlogs] = useState(null);
-  const[isPending,setIsPending] = useState(true);
- const [error,setError] =useState(null);
+ const { data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
-  useEffect(()=>{ 
-    setTimeout(()=>{
-    fetch(' http://localhost:3000/blogs')
-  .then(res => {
-    if(!res.ok){
-    throw Error("Could not fetch data from that resource");
-    }
-    return res.json();
-  })
-  .then((data=>{
-     setBlogs(data);
-     setIsPending(false);
-     setError(null);
-  })).catch(err=>{ 
-    setIsPending(false);
-    setError(err.message)})
-},1000);
-  },[]);
   return (
     <div className="home">
         {error && <div>{error}</div>}
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,11 @@
+import {Link} from'react-router-dom';
 const Navbar  = () => {
     return ( 
       <nav className="navbar">
         <h1>The Cris Blog</h1>
         <div className="links">
-            <a href="/">Home</a>
-            <a href="/create" style={{
-                color:'white',
-                backgroundColor:'#f1356d',
-                borderRadius:'8px'
-            }}>New blog</a>
+            <Link to="/">Home</Link>
+            <Link to="/create">New blog</Link>
         </div>
       </nav>  
      );
