831a11428b059286f358200b3014867a09c6a6cb
debcrisjol <debcrisjol@gmail.com>
2023-04-05 19:47:58 +0100

    404 pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -21,10 +22,14 @@ function App() {
         <Route path="/blogs/:id">
           <BlogDetails />    
         </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
        </Switch>
       </div>
     </div>
     </Router>
+
   );
 }
 
